cc351daea187c33e0d86c38864532f99fbe2ebce
hbb228 <hbb228@protonmail.com>
2023-05-07 08:39:21 +0100

    change deploy package name

--- deploy.nix
+++ deploy.nix
@@ -1,5 +1,5 @@
 { pkgs ? import ./pkgs.nix }:
 {
-    metamask-auth-service = pkgs.callPackage ./release.nix {};
+    custodial-wallet-api = pkgs.callPackage ./release.nix {};
     nodejs-16_x = pkgs.nodejs-16_x;
 }
